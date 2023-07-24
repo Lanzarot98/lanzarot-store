2e28ce6414ec94f800ffb84093107129f269485b
lanzarot <luis-389@hotmail.com>
2023-07-23 22:00:47 -0500

    params query

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -18,5 +18,30 @@ export class AppController {
   @Get('/ruta/')
   hello() {
     return 'Con /sas/';
+  }
+
+  @Get('products/filter')
+  getProductFilter() {
+    return `I am a filter`;
+  }
+
+  @Get('products/:productId')
+  getProduct(@Param('productId') productId: string) {
+    return `Product with id ${productId}`;
+  }
+
+  @Get('products')
+  getProducts(
+    @Query('limit') limit: number = 100,
+    @Query('offset') offset: number = 0,
+    @Query('brand') brand: string,
+  ) {
+    // const { limit, offset } = params;
+    return `Products limit=> ${limit} offset=> ${offset} brand=> ${brand}`;
+  }
+
+  @Get('categories/:id/products/:productId')
+  getCategory(@Param('productId') productId: string, @Param('id') id: string) {
+    return `Product with id ${productId} and ${id}`;
   }
 }
